52545bbd091e1e89f485c758e4648ac994700eff
Arun rPatap Singh <arunsingh28aug.as@gmail.com>
2022-02-21 19:39:09 +0530

    templates

--- src/Routes/api.ts
+++ src/Routes/api.ts
@@ -1,8 +1,12 @@
 import { Express, Request, Response } from 'express'
 
 
-export function router(router: Express) {
+export function apiRouter(router: Express) {
     router.post('/login', (req: Request, res: Response) => {
+        console.log('Data from front end:',req.body)
+    })
+
+    router.post('/register',(req: Request, res: Response)=>{
         console.log('Data from front end:',req.body)
     })
 }
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import { http } from '../config/http'
 import { router } from './Routes/index.routes';
+import { apiRouter } from './Routes/api';
 
 // server start config
 const app = express();
 http(app);
 // Routing
 router(app);
+
+// main apis
+apiRouter(app)
