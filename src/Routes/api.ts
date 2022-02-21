24699fd95ffdf8965526409b888b6cc7dcc94594
Arun rPatap Singh <arunsingh28aug.as@gmail.com>
2022-02-21 19:45:49 +0530

    data comming

--- src/Routes/api.ts
+++ src/Routes/api.ts
@@ -3,7 +3,12 @@ import { Express, Request, Response } from 'express'
 
 export function apiRouter(router: Express) {
     router.post('/login', (req: Request, res: Response) => {
-        console.log('Data from front end:',req.body)
+        const {email,password} = req.body
+        if(email === '' || password === ''){
+            return res.status(400).send({message: 'Please fill all fields'})
+        }else{
+            return res.status(200).send({message: 'Login Success'})
+        }
     })
 
     router.post('/register',(req: Request, res: Response)=>{
