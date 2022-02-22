93e854c6f330502a97cff03ee66432e81fe93dcf
Arun rPatap Singh <arunsingh28aug.as@gmail.com>
2022-02-22 23:56:37 +0530

    update token policy

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -7,15 +7,16 @@ export function apiRouter(router: Express) {
     router.post('/login', async (req: Request, res: Response) => {
         const { email, password } = req.body
         if (email === '' || password === '') {
-            return res.status(400).json({ message: 'Please fill all fields' })
+            return res.status(400).json({ message: 'Please fill all fields', auth: false })
         } else {
             const isUser = await (<IUser><unknown>_user).findUser(email, password);
             if (!isUser) {
-                return res.status(400).json({ message: 'Invalid email or password' })
+                return res.status(400).json({ message: 'Invalid email or password', auth: false })
             }
             // create token
-            const token = await getToken(isUser.id)
-            return res.status(200).json({ message: 'Login Success' })
+            const token = await getToken(isUser._id)
+            res.cookie('refreshToken', token, { httpOnly: true });
+            return res.status(200).json({ message: 'Login Success', auth: true })
         }
     })
 
