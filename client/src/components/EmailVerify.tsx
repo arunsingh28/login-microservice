8d787902055aedcfc7f32652e082c51d3f3881e3
Arun rPatap Singh <arunsingh28aug.as@gmail.com>
2022-03-13 21:03:31 +0530

    shmatic

--- client/src/components/EmailVerify.tsx
+++ client/src/components/EmailVerify.tsx
@@ -6,6 +6,21 @@ const EmailVerify = () => {
       <div className='email_wrapper'>
         <div className='email_header'>
           <h2>Arun' Auth</h2>
+          <p>Use your A Auth service Account</p>
+        </div>
+        <div className='email_form'>
+          <input type="text" placeholder='Email address' />
+          <div>
+            <a href="/">Forgot email ?</a>
+          </div>
+        </div>
+        <div className='other_btn'>
+          <div className='c_account'>
+            <a href="/">Create account</a>
+          </div>
+          <div className='next_btn'>
+            <button>Next</button>
+          </div>
         </div>
       </div>
     </div>
