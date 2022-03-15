6527adb1717041ba81c64c903eef894aac74b462
Arun rPatap Singh <arunsingh28aug.as@gmail.com>
2022-03-15 23:07:36 +0530

    Itoken interface

--- config/http.ts
+++ config/http.ts
@@ -3,10 +3,13 @@ import path from 'path'
 
 export function http(app: Express) {
 
-    // body parser for incoming data
+    // HTML template
     app.use(express.static(__dirname + '../src/template/public'));
     console.log(path.join(__dirname + '/../src/template/public/js'));
+    // body parser for json data
     app.use(express.json());
+    // body parser for text data
+    app.use(express.text());
     app.use(express.urlencoded({ extended: true }));
 
     const port = process.env.PORT || 80;
--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -2,8 +2,23 @@ import jwt from 'jsonwebtoken'
 import { key } from '../../config/keys'
 
 
+export interface Itoken {
+    id: string
+    iat: number
+    exp: number
+}
+
 export async function getToken(id: string) {
     return jwt.sign({ id }, key.JWT.SECRET, {
         expiresIn: key.JWT.EXPIRES
+    })
+}
+
+export async function verifyToken(token: string) {
+    return jwt.verify(token, key.JWT.SECRET, (err, decoded: Itoken) => {
+        if (err) {
+            return false
+        }
+        return decoded.id
     })
 }
