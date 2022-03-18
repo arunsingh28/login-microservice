4da485104da582341f7f532fe3e5aecb6fdb4704
Arun rPatap Singh <arunsingh28aug.as@gmail.com>
2022-03-18 18:08:56 +0530

    routing transition test

--- client/src/router/Router.tsx
+++ client/src/router/Router.tsx
@@ -1,15 +1,22 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import EmailVerify from '../components/EmailVerify';
 import PasswordVerify from '../components/PasswordVerify';
-
-
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
+import '../styles/App.scss'
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<EmailVerify />} />
-        <Route path='/password-verify' element={<PasswordVerify />} />
-      </Routes>
+      <TransitionGroup>
+        <CSSTransition
+          timeout={300}
+          classNames="fade"
+        >
+          <Routes>
+            <Route path='/' element={<EmailVerify />} />
+            <Route path='/password-verify' element={<PasswordVerify />} />
+          </Routes>
+        </CSSTransition>
+      </TransitionGroup>
     </BrowserRouter>
   )
 }
