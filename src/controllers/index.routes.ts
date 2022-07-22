a461a965693efab25a1a54f8f3cde4691daea97e
arunsingh28 <arunsingh28aug.as@gmail.com>
2022-07-22 06:56:39 -0700

    update some change

--- src/controllers/index.routes.ts
+++ src/controllers/index.routes.ts
@@ -4,12 +4,12 @@ import path from 'path'
 export function router(router: Express) {
     // show baisc login page
     router.get('/', (req: Request, res: Response) => {
-        res.sendFile(path.join(__dirname, '../template/login.html'));
+        res.sendFile(path.join(__dirname, '../../template/login.html'));
     })
     router.get('/create', (req: Request, res: Response) => {
-        res.sendFile(path.join(__dirname, '../template/signup.html'));
+        res.sendFile(path.join(__dirname, '../../template/signup.html'));
     })
-    router.get('/mini',(req: Request, res: Response) => {
-        res.sendFile(path.join(__dirname, '../template/mini.html'));
+    router.get('/mini', (req: Request, res: Response) => {
+        res.sendFile(path.join(__dirname, '../../template/mini.html'));
     })
 }
